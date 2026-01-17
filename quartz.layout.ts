9f22c93d24dfe01376316e744f0039eabe6db2a2
Kotyga Maiia <kotmayyaka@gmail.com>
2026-01-17 14:27:48 +0300

    Quartz sync: Jan 17, 2026, 2:27 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/Kotyga/botay-ds",
+      Telegram: "https://t.me/maia_llm",
+      LinkedIn: "https://www.linkedin.com/in/maiia-kotyga-358791245/"
     },
   }),
 }
